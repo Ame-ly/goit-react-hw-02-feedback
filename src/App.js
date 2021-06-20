c3deebfc025f453a914d7fd48774612cd855cc69
Svitlana Niesmielova <amelia-com@hotmail.com>
2021-06-20 21:53:44 +0300

    update markup

--- src/App.js
+++ src/App.js
@@ -49,13 +49,15 @@ class App extends Component {
         </Section>
 
         <Section title="Statistics">
-          {total > 0 ? <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          /> :
+          {total > 0
+            ? <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+            :
             <Notification message={"No feedback given"} />}
         </Section>
       </>
